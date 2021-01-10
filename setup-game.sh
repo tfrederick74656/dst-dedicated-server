f06a6b9ef217b981ae7ff5cd99d7601a52db446d
Tyler James Frederick <tfrederick74656@gmail.com>
2021-01-09 23:44:53 -0500

    Move template cluster configs to inline variables

--- setup-game.sh
+++ setup-game.sh
@@ -1,25 +1,3 @@
-# Define directory names
-root_dir=/opt/dst-server
-setup_dir=$root_dir/game-setup
-
-# Create root directory
-sudo mkdir $root_dir
-sudo chown ec2-user:ec2-user $root_dir
-
-# Create subdirectories
-mkdir $setup_dir
-
-# Copy scripts and config files to the setup directory
-cp -R * $setup_dir
-cd $setup_dir
-
-# Define cluster name and cluster token
-echo -n "Cluster token: "
-read cluster_token
-echo $cluster_token > templates/cluster_token.txt
-
-# Start the game servers
-#sudo docker-compose -f docker-compose_lib.yml up
-#sudo docker-compose -f docker_compose_steam.yml up
-#sudo docker-compose -f docker_compose_setup.yml up
-#sudo docker-compose -f docker-compose_game.yml up
+# Setup a server from scratch
+sudo docker-compose up lib steam setup
+sudo docker-compose up master caves
